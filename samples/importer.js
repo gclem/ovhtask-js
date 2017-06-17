5bc4ee792aa1b635483078393306a2ef1d3bc104
gclement <gclement@LAPTOP777362.local>
2017-06-17 03:09:12 -0400

    Fix : importer console.log

--- samples/importer.js
+++ samples/importer.js
@@ -12,7 +12,7 @@ console.log("OVH Task CLI Importer.");
  //// All 
 if (program.all) {
     importer.importAll({ withDetails : false })
-        .then((datas) => { console.log('%s items imported.', datas.length); process.exit(0); })
+        .then((datas) => { console.log('%s items imported.', datas[0].length); process.exit(0); })
         .catch((err) => { console.error('Full imported faulted', err); process.exit(1); });
 }
 
@@ -20,7 +20,7 @@ if (program.all) {
 if (program.refresh) {
     importer
         .import(0, { from : 1, to : 3, withDetails : true })
-        .then((datas) => { console.log('Refreshed. %s items imported.', datas.length); process.exit(0); })
+        .then((datas) => { console.log('Refreshed. %s items imported.', datas[0].length); process.exit(0); })
         .catch((err) => { console.error('Refresh faulted', err); process.exit(1); });
 }
 
